6dce824b21e71016055042a23509405c573c46cb
alexeusgr <alexeusgr@gmail.com>
2022-10-24 09:17:43 +0300

    pure tasty

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,36 @@
-module Main where
---import Test.Plutip.Internal.LocalCluster (startCluster, stopCluster)
+import Test.Tasty
+import Test.Tasty.SmallCheck as SC
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [smallCheckTests, quickCheckTests, unitTests]
+
+smallCheckTests :: TestTree
+smallCheckTests = testGroup "SmallCheck Tests"
+  [ SC.testProperty "String length <= 3" $
+      \s -> length (take 3 (s :: String)) <= 3
+  , SC.testProperty "String length <= 2" $  -- should fail
+      \s -> length (take 3 (s :: String)) <= 2
+  ]
+
+quickCheckTests :: TestTree
+quickCheckTests = testGroup "QuickCheck Tests"
+  [ QC.testProperty "String length <= 5" $
+      \s -> length (take 5 (s :: String)) <= 5
+  , QC.testProperty "String length <= 4" $  -- should fail
+      \s -> length (take 5 (s :: String)) <= 4
+  ]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit Tests"
+  [ testCase "String comparison 1" $
+      assertEqual "description" "OK" "OK"
+
+  , testCase "String comparison 2" $  -- should fail
+      assertEqual "description" "fail" "fail!"
+  ]
