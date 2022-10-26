64ee0931143558611616b030608baa4f2b1a77a7
alexeusgr <alexeusgr@gmail.com>
2022-10-26 09:47:22 +0300

    separate tests from the program

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,10 @@
-import Test.Tasty
-import Test.Tasty.SmallCheck as SC
-import Test.Tasty.QuickCheck as QC
-import Test.Tasty.HUnit
+-- Copyright (c) 2006 Don Stewart - http://www.cse.unsw.edu.au/~dons/
+-- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
+--
+import System.Environment
 
-
+-- | 'main' runs the main program
 main :: IO ()
-main = defaultMain tests
+main = getArgs >>= print . haqify . head
 
-tests :: TestTree
-tests = testGroup "Tests" [smallCheckTests, quickCheckTests, unitTests]
-
-smallCheckTests :: TestTree
-smallCheckTests = testGroup "SmallCheck Tests"
-  [ SC.testProperty "String length <= 3" $
-      \s -> length (take 3 (s :: String)) <= 3
-  , SC.testProperty "String length <= 2" $  -- should fail
-      \s -> length (take 3 (s :: String)) <= 2
-  ]
-
-quickCheckTests :: TestTree
-quickCheckTests = testGroup "QuickCheck Tests"
-  [ QC.testProperty "String length <= 5" $
-      \s -> length (take 5 (s :: String)) <= 5
-  , QC.testProperty "String length <= 4" $  -- should fail
-      \s -> length (take 5 (s :: String)) <= 4
-  ]
-
-unitTests :: TestTree
-unitTests = testGroup "Unit Tests"
-  [ testCase "String comparison 1" $
-      assertEqual "description" "OK" "OK"
-
-  , testCase "String comparison 2" $  -- should fail
-      assertEqual "description" "fail" "fail!"
-  ]
+haqify s = "Haq! " ++ s
