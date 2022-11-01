ffa6b3e555c9a76ceef5b1aef3567f65dc5fa607
alexeusgr <alexeusgr@gmail.com>
2022-11-01 08:51:04 +0200

    fixes

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ import Test.Plutip.Internal.LocalCluster (startCluster, stopCluster)
 import Test.Plutip.Internal.BotPlutusInterface.Wallet (BpiWallet, addSomeWallet)
 import Test.Plutip.LocalCluster (waitSeconds)
 import Test.Plutip.Internal.Types
+import qualified Plutus.Contract as Contract
+import qualified Ledger.Constraints as Constraints
 
 setup :: ReaderT ClusterEnv IO (ClusterEnv, BpiWallet)
 setup = do
@@ -15,8 +17,6 @@ setup = do
 addWalletWithAdas :: [Ada] -> ReaderT ClusterEnv IO BpiWallet
 addWalletWithAdas = addSomeWallet . map (fromInteger . Ada.toLovelace)
 
-import qualified Plutus.Contract as Contract
-import qualified Ledger.Constraints as Constraints
 
 -- | Pay 5 ada to yourself ...very useful thing to do
 payMyself :: AsContractError e => Contract w s e ()
@@ -49,13 +49,6 @@ data FailureReason e
   | -- | exception caught during contract execution
     CaughtException SomeException
   deriving stock (Show)
-
-ExecutionResult exOutcome _ _ <- runContract @() @EmptySchema @Text cEnv ownWallet payMyself
-
-case exOutcome of
-  Left (ContractExecutionError e) -> putStrLn "Contract failed" >> print e
-  Left (CaughtException e) -> putStrLn "Unexpected exception" >> print e
-  Right _ -> pure ()
 
 main :: IO ()
 main = do
